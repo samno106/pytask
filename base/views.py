5ca41103e4983da5af95f53684bd83cd2cfeeabc
Khit Samno <khitsamno@gmail.com>
2025-05-27 00:03:02 +0700

    start project and app

--- base/views.py
+++ base/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Task
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    tasks = Task.objects.all()
+    context = {"tasks": tasks}
+    return render(request, "views/home/index.html", context)
