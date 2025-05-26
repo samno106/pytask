b543d55147e7cf52117f2c4840744d799d46e8ac
Khit Samno <khitsamno@gmail.com>
2025-05-26 22:41:08 +0700

    init commit

---
+++ base/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    class TaskStatus(models.TextChoices):
+        PENDING = "PENDING"
+        PROGRESS = "PROGRESS"
+        COMPLETED = "COMPLETED"
+    title =models.CharField(max_length=255)
+    desc = models.TextField()
+    status= models.CharField(choices=TaskStatus.choices,default=TaskStatus.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+   
